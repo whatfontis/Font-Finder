7344185675be4eaf09f8c1b21db7da269e95498d
whatfontis <47185194+whatfontis@users.noreply.github.com>
2024-01-24 15:06:59 +0200

    Update example.php

--- example.php
+++ example.php
@@ -4,7 +4,7 @@ www.WhatFontIs.com
 
 Find any font from any image (commercial or free)
 
-Using a catalogue of 500k+ fonts (commercial or free) and font finder AI, for every image uploaded we show over 60 similar fonts ( free or commercial).
+Using a catalogue of 990K+ fonts (commercial or free) and font finder AI, for every image uploaded we show over 60 similar fonts ( free or commercial).
 
 More info here:
 
